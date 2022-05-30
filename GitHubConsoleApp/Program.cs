342b1dd11d8cd9a26034e7460569876a0926363b
Shivam-256 <99988045+Shivam-256@users.noreply.github.com>
2022-05-30 12:55:34 +0530

    Update Program.cs

--- GitHubConsoleApp/Program.cs
+++ GitHubConsoleApp/Program.cs
@@ -7,6 +7,17 @@ namespace GitHubConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Add(123, 22);
+            Subtract(222, 211);
+            Console.WriteLine( "end of program");
+        }
+        static void Add(int num1, int num2)
+        {
+            Console.WriteLine("Sum is {0}", num1+num2);
+        }
+        static void Subtract(int num1, int num2)
+        {
+            Console.WriteLine("Diff. is {0}", num1 - num2);
         }
     }
 }
